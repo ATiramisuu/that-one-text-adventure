bc93cc8256bde4b75e13ccbbeb2b3d2b59bd06c8
CaffeMacchiato <101424229+CaffeMacchiato@users.noreply.github.com>
2024-07-10 14:16:50 -0700

    Update startScreen.py
    🥇

--- startScreen.py
+++ startScreen.py
@@ -1,24 +1,59 @@
+import firstArea
+import saveProgress
+import os
+
+print("Welcome to That One Text Adventure")  # Welcome the player to your game --> non-repeatable line
 
 
-def menu():
-    print("Welcome to That One Text Adventure")
-    print("1. Start New Game")
-    print("2. Continue Game")
-    print("3. Exit Game")
-    
+def menu():  # Create a menu with different options
+    print("\nPlease choose 1 of the following options:",
+          "1. Start new game",
+          "2. Continue game",
+          "3. Exit", sep="\n", end="\n\n")
+
 
 def main():
-    menu()
-    option = int(input("select option to start: "))
-    if option == 1:
-        print("New Game Started")
-        import firstArea
-        firstArea.startGame()
-    elif option == 2:
-        print("Finding Game file...")
+    while True:
+        menu()  # Run the menu function
+        game_state = saveProgress.load_game()
 
-    elif option == 3:
-        print("Exiting Game")
-        exit(1)
+        try:
+            option = int(input("Select option to start: "))
+        except ValueError:  # If we get a ValueError from invalid input
+            print("Invalid input. Please enter a number between 1 and 3.")
+            continue
 
-main()
+        if option == 1:  # We want to start a new game
+            if game_state:  # Check if there is existing save data
+                wipe = input("Existing save data found. Do you want to wipe the data and start over? (yes/no): ").strip().lower()
+                if wipe == "yes":
+                    os.remove("saveData.txt")
+                    print("Save data wiped. Starting a new game!")
+                    firstArea.startGame()
+                    break # Exit the while loop
+                else:
+                    print("Okay, going back to the main menu now.")  # you'll get booted back out to the main menu
+            else:
+                print("Starting a new game!")  # This starts a new game
+                firstArea.startGame()
+                break  # Exit the while loop
+
+        elif option == 2:  # We want to continue our game
+            if not game_state:  # We haven't started a new game yet or save file is empty
+                print("You haven't started a new game or no save data found! Please start a new game first.")
+            else:  # We have already started a new game
+                print("Finding save file...")
+                print("Save file found! Continuing your game")
+                firstArea.continueArea(game_state)
+                break  # Exit the while loop
+
+        elif option == 3:  # We want to exit the program
+            print("Exiting the program now")
+            exit(1)
+
+        else:  # Our user input is invalid
+            print("Your input is invalid, please try again")
+
+
+if __name__ == "__main__":
+    main()
