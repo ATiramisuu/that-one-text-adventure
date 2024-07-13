425e5abbfb98a27df78054b2265db808788f1197
CaffeMacchiato <101424229+CaffeMacchiato@users.noreply.github.com>
2024-07-12 22:29:54 -0700

    Update firstArea.py
    added classes

--- firstArea.py
+++ firstArea.py
@@ -1,4 +1,4 @@
-import saveProgress
+from game_classes import GameState, Player
 import portShop
 
 
@@ -7,50 +7,44 @@ def readScript(start_line, end_line):
         with open("portScript.txt", 'r') as file:
             lines = file.readlines()
             specific_lines = lines[start_line - 1:end_line]
-
             for line in specific_lines:
                 print(line.strip())
-
             print("- " * 40)  
-
     except Exception as e:
         print(f"An error occurred: {e}")
 
+# -------------------------------------------------------------------------------------
 
 def startGame():
     print("New game started!\n")
     name = input("Enter your name: ")
-    game_state = {"name": name}
-    saveProgress.save_game(game_state)
+    game_state = GameState(name)
+    game_state.save()
     print("Hello " + name)
     startArea()
 
+# -------------------------------------------------------------------------------------
 
 def startArea():
     readScript(1, 6)
-
     while True:
-        readScript(6, 10) # we're going to print the options before asking for input EVERY TIME
+        readScript(6, 10)
         option = input("\nWhat would you like to do? ")
-
         if option == "1":
             readScript(10, 16)
-
         elif option == "2":
-            #readScript(21, 30)
             print("Let's go to the general store!\n")
             portShop.welcomeShop()
             break
-
-
         elif option == "3":
             readScript(31, 40)
             break
-
         else: 
             print("Invalid input! Select a valid option.")
 
+# -------------------------------------------------------------------------------------
 
 def continueArea(game_state):
-    print("\nContinuing game with previous save data \nWelcome back " + game_state['name'])
+    print("\nContinuing game with previous save data \nWelcome back " + game_state.player_name)
     startArea()
+
